11a810fb35e92d29146f881472341838cead2758
codaamok <github@adamcook.io>
2021-12-05 12:02:37 +0000

    Day 4 p2

--- 2021/aoc-day4.ps1
+++ 2021/aoc-day4.ps1
@@ -1,25 +1,41 @@
-# Part 1
+function Get-DeepClone
+{
+    # https://powershellexplained.com/2016-11-06-powershell-hashtable-everything-you-wanted-to-know-about/#deep-copies
+    [cmdletbinding()]
+    param(
+        $InputObject
+    )
+    process
+    {
+        if($InputObject -is [hashtable]) {
+            $clone = @{}
+            foreach($key in $InputObject.keys)
+            {
+                $clone[$key] = Get-DeepClone $InputObject[$key]
+            }
+            return $clone
+        } else {
+            return $InputObject
+        }
+    }
+}
 
 $data = Get-Content $pwd\aoc-day4.txt
-
 $bingoNumbers = $data[0] -split ','
-
 $lines = ($data[2..$data.Count]).Where{ -not [String]::IsNullOrWhiteSpace($_) }
 
 $boards = for ($l = 0; $l -lt $lines.Count; $l++) {
     if ($l % 5 -eq 0) {
-        $board = @()
+        $board = @{}
     }
 
     $numbers = $lines[$l].Split(' ').Where{ -not [String]::IsNullOrEmpty($_) }
 
     for ($n = 0; $n -lt 5; $n++) {
-        $board += [ordered]@{
-            $numbers[$n] = [PSCustomObject]@{
-                row    = $l % 5 + 1
-                col    = $n + 1
-                marked = $false
-            }
+        $board[[int]$numbers[$n]] = @{
+            row    = $l % 5 + 1
+            col    = $n + 1
+            marked = $false
         }
     }
     
@@ -28,22 +44,33 @@ $boards = for ($l = 0; $l -lt $lines.Count; $l++) {
     }
 }
 
-:FindFirstBoardToWin foreach ($bingoNumber in $bingoNumbers) {
-    foreach ($board in $boards) {
-        if ($board.$bingoNumber) {
-            ($board.$bingoNumber).marked = $true
+1..$boards.Count | ForEach-Object -Begin {
+    $boardWinningStatus = @{}
+} -Process {
+    $boardWinningStatus[$_] = $false
+}
+
+:parentloop foreach ($bingoNumber in $bingoNumbers) {
+    for ($b = 0; $b -lt 100; $b++) {
+        $board = $boards[$b]
+        if ($board[[int]$bingoNumber]) {
+            ($board[[int]$bingoNumber]).marked = $true
 
             # Check to see if this board 'has bingo' since a new number has been marked as true
             foreach ($n in 1..5) {
                 foreach ($item in "row","col") {
-                    if ($board.GetEnumerator().values.Where{$_.$item -eq $n -And $_.marked -eq $true}.count -eq 5) {
-                        $unmarkedNumbersSum = 0
-                        foreach ($key in $board.keys) {
-                            if (-not ($board.$key).marked) {
-                                $unmarkedNumbersSum += [int]$key
-                            }
+                    if ($board.values.Where{$_.$item -eq $n -And $_.marked -eq $true}.count -eq 5) {
+                        $boardWinningStatus[$b+1] = $true
+                        $numOfWinningBoards = ($boardWinningStatus.Values -match "True").count
+                        if ($numOfWinningBoards -eq 1) {
+                            $firstBoard = Get-DeepClone $boards[$b]
+                            $firstBoardBingoNumber = $bingoNumber
                         }
-                        break FindFirstBoardToWin
+                        elseif ($numOfWinningBoards -eq $boards.Count) {
+                            $lastBoard = Get-DeepClone $boards[$b]
+                            $lastBoardBingoNumber = $bingoNumber
+                            break parentloop
+                        }
                     }
                 }
             }
@@ -51,6 +78,19 @@ $boards = for ($l = 0; $l -lt $lines.Count; $l++) {
     }
 }
 
-"Board {0} was the first to win" -f $boards.IndexOf($board) | Write-Host
+function Get-FinalScore {
+    param (
+        [Object]$Board,
+        [int]$BingoNumber
+    )
+    foreach ($item in $Board.GetEnumerator().Where{-not $_.value.marked}) {
+        $unmarkedSum += [int]$item.key
+    }
+    $unmarkedSum * $BingoNumber
+}
 
-$unmarkedNumbersSum * $bingoNumber
+# Part 1 answer
+"First winning board final score: {0}" -f (Get-FinalScore $firstBoard $firstBoardBingoNumber)
+
+# Part 2 answer
+"Last winning board final score: {0}" -f (Get-FinalScore $lastBoard $lastBoardBingoNumber)
