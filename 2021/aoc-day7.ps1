6088e4f5f4e19d8a2caaa3d3705aa529e5ab730c
codaamok <me@cookadam.co.uk>
2021-12-07 19:39:34 +0000

    Slow Day 7 part 2

--- 2021/aoc-day7.ps1
+++ 2021/aoc-day7.ps1
@@ -1,3 +1,5 @@
+# Part 1
+
 [int[]]$crabs = (Get-Content $pwd\aoc-day7.txt) -split ','
 
 [int]$min = ($crabs | Measure-Object -Minimum).Minimum
@@ -7,6 +9,23 @@ $allfuel = for ($p = $min; $p -lt $max+1; $p++) {
     $fuel = 0
     foreach ($crab in $crabs) {
         $fuel += [Math]::Abs($p - $crab)
+    }
+    $fuel
+}
+
+($allfuel | Measure-Object -Minimum).Minimum
+
+# Part 2
+
+[int[]]$crabs = (Get-Content $pwd\aoc-day7.txt) -split ','
+
+[int]$min = ($crabs | Measure-Object -Minimum).Minimum
+[int]$max = ($crabs | Measure-Object -Maximum).Maximum
+
+$allfuel = for ($p = $min; $p -lt $max+1; $p++) {
+    $fuel = 0
+    foreach ($crab in $crabs) {
+        $fuel += (1..[Math]::Abs($p - $crab - 1) | Measure-Object -Sum).Sum
     }
     $fuel
 }
