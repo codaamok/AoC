6a6cd37a9ab9afc81ebc5fecc35b6f4c08c15908
codaamok <github@adamcook.io>
2021-12-11 19:16:07 +0000

    Day 10 part 2

--- 2021/aoc-day10.ps1
+++ 2021/aoc-day10.ps1
@@ -1,56 +1,116 @@
 # Part 1
 
-# Everything within a chunk must resolve
-# If chunk length is zero, every encapsulating bracket in the same direction must resolve
-# e.g. [([])) is corruption
-# e.g. ({<>}< is *not* corruption
-
-# ^ not sure if that's true... what if I find all single chunks, and work outwards?
+function Remove-Chunks {
+    param(
+        [String]$String
+    )
+    $r = '\[\]|\{\}|<>|\(\)'
+    while ($String -match $r) {
+        $String = $String -replace $r
+    }
+    if ($String.Length -eq 0) { $true }
+    else { $String }
+}
 
 $navigationSubsystem = Get-Content .\aoc-day10.txt
-$incompleteLines = @()
-$corruptLines = @()
 
-$pairs = @{
+$Score = @{
+    ")" = 3
+    "]" = 57
+    "}" = 1197
+    ">" = 25137
+}
+
+$symbols = @{
     "{" = "}"
     "[" = "]"
     "<" = ">"
     "(" = ")"
 }
 
-foreach ($line in $navigationSubsystem) {
-    for ($i = 0; $i -lt $line.length; $i++) {
-        for ($y = $i+1; $y -lt $line.length; $y++) {
-            $ignore = 0
-            switch ($line[$y]) {
-                $line[$i] { 
-                    $ignore++ 
-                }
-                $pairs[$chair] { 
-                    # deduce corruption
-                }
-                default {
+$corruptLines = @()
+$illegalChars = @()
+$incompleteLines = @()
 
+foreach ($line in $navigationSubsystem) {
+    $foundCorruptLine = $false
+    $encounteredChars = @{}
+
+    $tmpLine = $line
+    $tmpLine = Remove-Chunks $tmpLine
+
+    :mybreak for ($s = 0; $s -lt $tmpLine.Length; $s++) {
+        if ($symbols.Values -contains $tmpLine[$s]) {
+            $illegalChars += $Score[[string]$tmpLine[$s]]
+            $foundCorruptLine = $true
+            $corruptLines += $line
+            break
+        }
+
+        if ($encounteredChars[$tmpLine[$s]]) {
+            continue
+        }
+        else {
+            $encounteredChars[$tmpLine[$s]] += 1
+            for ($f = $s + 1; $f -lt $tmpLine.Length; $f++) {
+                if ($tmpLine[$f] -eq $symbols[$tmpLine[$s]]) {
+                    $result = Remove-Chunks $tmpLine.Substring($s, $f)
+                    if ($result -eq $true) {
+                        $s = $f + 1
+                        break
+                    }
+                    else {
+                        # Find first illegal character
+                        foreach ($char in [char[]]$result) {
+                            if ($symbols.Values -contains [string]$char) {
+                                $illegalChars += $Score[[string]$char]
+                                break
+                            }
+                        }
+                        $corruptLines += $line
+                        $foundCorruptLine = $true
+                        break mybreak
+                    }
                 }
             }
         }
     }
-    
-    switch -Regex([char[]]$line) {
-        "\[|\{|<|\(" {
-            $symbols[$_] += 1
-        }
-        "\]|\}|>|\)" {
-            $symbols[$_] -= 1
-        }
-    }
 
-    foreach ($key in $symbols.Keys) {
-        if ($symbols[$key] -ne 0) {
-            $incompleteLines += $line
-        }
-        else {
-            $corruptLines += $line
-        }
+    if (-not $foundCorruptLine) {
+        $incompleteLines += $line
     }
 }
+
+$sum = 0
+$illegalChars | Group-Object -NoElement | ForEach-Object {
+    $sum += [int]$_.count * [int]$_.name
+}
+$sum
+
+# Part 2
+
+$Score = @{
+    ")" = 1
+    "]" = 2
+    "}" = 3
+    ">" = 4
+}
+
+$Scores = foreach ($line in $incompleteLines) {
+    $tmpLine = Remove-Chunks $line
+    $completingChars = @()
+    for ($f = $tmpLine.Length - 1; $f -ne -1; $f--) {
+        $completingChars += $symbols[[string]$tmpLine[$f]]
+        $tmpLine += $symbols[[string]$tmpLine[$f]]
+    }
+
+    $sum = 0
+    foreach ($char in $completingChars) {
+        $sum = $sum * 5
+        $sum += $Score[[string]$char]
+    }
+    $sum
+}
+
+$index = ($Scores.count - 1) / 2
+($Scores | Sort-Object)[$index]
